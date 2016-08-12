766a5bb6826f26ce49df178f52d2d303fa68d762
Magnusito <magnus_7@hotmail.es>
2016-08-13 00:28:32 +0200

    Estus Flask now give Empty Estus Flask on usage

---
+++ stats/effects/heal/estusflaskheal.lua
@@ -0,0 +1,18 @@
+function init()
+  animator.setParticleEmitterOffsetRegion("healing", mcontroller.boundBox())
+  animator.setParticleEmitterEmissionRate("healing", config.getParameter("emissionRate", 3))
+  animator.setParticleEmitterActive("healing", true)
+
+  script.setUpdateDelta(5)
+
+  self.healingRate = config.getParameter("healAmount", 30) / effect.duration()
+  world.spawnItem("DaS_emptyflask", mcontroller.position(), 1)
+end
+
+function update(dt)
+  status.modifyResource("health", self.healingRate * dt)
+end
+
+function uninit()
+
+end
